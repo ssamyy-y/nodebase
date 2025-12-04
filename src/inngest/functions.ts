2e94cbdb7c0236aaf20be65312cb4d6ea5088948
Ssamyyy <shauryasingh1040@gmail.com>
2025-12-04 13:20:35 +0530

    08:error tracking

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -2,17 +2,29 @@ import prisma from "@/lib/db";
 import { inngest } from "./client";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { generateText } from "ai";
-
+import * as Sentry from "@sentry/nextjs";
 const google = createGoogleGenerativeAI();
 
 export const execute = inngest.createFunction(
   { id: "execute" },
   { event: "execute/ai" },
   async ({ event, step }) => {
+    Sentry.logger.info("User triggered test log", {
+      log_source: "sentry_test",
+    });
+
+    console.warn("warning");
+    console.error("error i wanna track broski");
+
     const { steps } = await step.ai.wrap("gemini-generate-text", generateText, {
       model: google("gemini-2.5-flash"),
       system: "You are a helpful assistant.",
       prompt: "what is 2+2?",
+      experimental_telemetry: {
+        isEnabled: true,
+        recordInputs: true,
+        recordOutputs: true,
+      },
     });
 
     return steps;
